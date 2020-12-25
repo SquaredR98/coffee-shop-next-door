6e9539ccd0ae716ef98766f574e747919488dd38
Ravi Ranjan <yourfriendraviranjan@gmail.com>
2020-12-25 11:53:19 +0530

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact />
+        </Switch>
+      </Router>
     </div>
   );
 }
