28dbe2bd53b5bf46cd79131147a65f2e616d2545
SquaredR98 <mail@ravi-ranjan.in>
2023-10-06 10:45:23 +0530

    id base routing

--- pages/coffee-store/[id].tsx
+++ pages/coffee-store/[id].tsx
@@ -2,12 +2,13 @@ import React from "react";
 import { useRouter } from "next/router";
 
 import coffeeStores from "../../data/coffee-stores.json";
+import Link from "next/link";
 
-export function getStaticProps({ params }: { params: { id: number } }) {
+export function getStaticProps({ params }: { params: { id: string } }) {
   return {
     props: {
       coffeeStore: coffeeStores.find(
-        (coffeeStore) => coffeeStore.id === params.id
+        (coffeeStore) => coffeeStore.id.toString() === params.id
       ),
     },
   };
@@ -15,12 +16,17 @@ export function getStaticProps({ params }: { params: { id: number } }) {
 
 export function getStaticPaths() {
   return {
-    paths: [{ params: { id: 0 } }, { params: { id: 1 } }],
+    paths: [{ params: { id: '0' } }, { params: { id: '1' } }],
+    fallback: false,
   };
 }
 
-const CoffeeStore = () => {
-  return <div>CoffeeStore</div>;
+const CoffeeStore = ({ coffeeStore }: { coffeeStore: any }) => {
+  return <div>
+    <Link href="/">Home</Link>
+    <h1>Coffee Store: {coffeeStore.name}</h1>
+    <p>Coffee Store Address: {coffeeStore.address}</p>
+  </div>;
 };
 
 export default CoffeeStore;
